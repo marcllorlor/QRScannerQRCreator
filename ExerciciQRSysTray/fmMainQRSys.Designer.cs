cae566a9aa9bdcf2b8ddf96a457098a31f8f365a
Marc Llorca <marcllorlor@gmail.com>
2023-06-01 12:59:46 +0200

    Deploy

---
+++ ExerciciQRSysTray/fmMainQRSys.Designer.cs
@@ -0,0 +1,168 @@
+﻿namespace ExerciciQRSysTray
+{
+    partial class fmMainQRSys
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(fmMainQRSys));
+            this.niIcona = new System.Windows.Forms.NotifyIcon(this.components);
+            this.cmMenuStripIcona = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.activarCamaraToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.desactivarCamaraToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.generarQRToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.sortirToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.lbText = new System.Windows.Forms.Label();
+            this.btCrearQR = new System.Windows.Forms.Button();
+            this.tbQR = new System.Windows.Forms.TextBox();
+            this.imgQR = new System.Windows.Forms.PictureBox();
+            this.cmMenuStripIcona.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.imgQR)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // niIcona
+            // 
+            this.niIcona.ContextMenuStrip = this.cmMenuStripIcona;
+            this.niIcona.Icon = ((System.Drawing.Icon)(resources.GetObject("niIcona.Icon")));
+            this.niIcona.Text = "Programa per detectar QR";
+            this.niIcona.Visible = true;
+            this.niIcona.MouseClick += new System.Windows.Forms.MouseEventHandler(this.niIcona_MouseClick);
+            // 
+            // cmMenuStripIcona
+            // 
+            this.cmMenuStripIcona.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.cmMenuStripIcona.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.activarCamaraToolStripMenuItem,
+            this.desactivarCamaraToolStripMenuItem,
+            this.generarQRToolStripMenuItem,
+            this.sortirToolStripMenuItem});
+            this.cmMenuStripIcona.Name = "cmMenuStripIcona";
+            this.cmMenuStripIcona.Size = new System.Drawing.Size(199, 100);
+            // 
+            // activarCamaraToolStripMenuItem
+            // 
+            this.activarCamaraToolStripMenuItem.Name = "activarCamaraToolStripMenuItem";
+            this.activarCamaraToolStripMenuItem.Size = new System.Drawing.Size(198, 24);
+            this.activarCamaraToolStripMenuItem.Text = "ActivarCamara";
+            this.activarCamaraToolStripMenuItem.Click += new System.EventHandler(this.activarCamaraToolStripMenuItem_Click);
+            // 
+            // desactivarCamaraToolStripMenuItem
+            // 
+            this.desactivarCamaraToolStripMenuItem.Name = "desactivarCamaraToolStripMenuItem";
+            this.desactivarCamaraToolStripMenuItem.Size = new System.Drawing.Size(198, 24);
+            this.desactivarCamaraToolStripMenuItem.Text = "DesactivarCamara";
+            this.desactivarCamaraToolStripMenuItem.Click += new System.EventHandler(this.desactivarCamaraToolStripMenuItem_Click);
+            // 
+            // generarQRToolStripMenuItem
+            // 
+            this.generarQRToolStripMenuItem.Name = "generarQRToolStripMenuItem";
+            this.generarQRToolStripMenuItem.Size = new System.Drawing.Size(198, 24);
+            this.generarQRToolStripMenuItem.Text = "GenerarQR";
+            this.generarQRToolStripMenuItem.Click += new System.EventHandler(this.generarQRToolStripMenuItem_Click);
+            // 
+            // sortirToolStripMenuItem
+            // 
+            this.sortirToolStripMenuItem.Name = "sortirToolStripMenuItem";
+            this.sortirToolStripMenuItem.Size = new System.Drawing.Size(198, 24);
+            this.sortirToolStripMenuItem.Text = "Sortir";
+            this.sortirToolStripMenuItem.Click += new System.EventHandler(this.sortirToolStripMenuItem_Click);
+            // 
+            // lbText
+            // 
+            this.lbText.AutoSize = true;
+            this.lbText.Location = new System.Drawing.Point(154, 27);
+            this.lbText.Name = "lbText";
+            this.lbText.Size = new System.Drawing.Size(104, 16);
+            this.lbText.TabIndex = 8;
+            this.lbText.Text = "Introdueix un text";
+            // 
+            // btCrearQR
+            // 
+            this.btCrearQR.BackColor = System.Drawing.Color.YellowGreen;
+            this.btCrearQR.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btCrearQR.Location = new System.Drawing.Point(542, 74);
+            this.btCrearQR.Name = "btCrearQR";
+            this.btCrearQR.Size = new System.Drawing.Size(105, 33);
+            this.btCrearQR.TabIndex = 7;
+            this.btCrearQR.Text = "Crear QR";
+            this.btCrearQR.UseVisualStyleBackColor = false;
+            this.btCrearQR.Click += new System.EventHandler(this.btCrearQR_Click);
+            // 
+            // tbQR
+            // 
+            this.tbQR.Location = new System.Drawing.Point(153, 46);
+            this.tbQR.Name = "tbQR";
+            this.tbQR.Size = new System.Drawing.Size(494, 22);
+            this.tbQR.TabIndex = 6;
+            // 
+            // imgQR
+            // 
+            this.imgQR.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.imgQR.Location = new System.Drawing.Point(153, 74);
+            this.imgQR.Name = "imgQR";
+            this.imgQR.Size = new System.Drawing.Size(350, 350);
+            this.imgQR.TabIndex = 5;
+            this.imgQR.TabStop = false;
+            // 
+            // fmMainQRSys
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1253, 450);
+            this.Controls.Add(this.lbText);
+            this.Controls.Add(this.btCrearQR);
+            this.Controls.Add(this.tbQR);
+            this.Controls.Add(this.imgQR);
+            this.Name = "fmMainQRSys";
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
+            this.Text = "CodiExemple";
+            this.WindowState = System.Windows.Forms.FormWindowState.Minimized;
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.fmMainQRSys_FormClosing);
+            this.Load += new System.EventHandler(this.fmMainQRSys_Load);
+            this.cmMenuStripIcona.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.imgQR)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.NotifyIcon niIcona;
+        private System.Windows.Forms.ContextMenuStrip cmMenuStripIcona;
+        private System.Windows.Forms.ToolStripMenuItem activarCamaraToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem desactivarCamaraToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem generarQRToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem sortirToolStripMenuItem;
+        private System.Windows.Forms.Label lbText;
+        private System.Windows.Forms.Button btCrearQR;
+        private System.Windows.Forms.TextBox tbQR;
+        private System.Windows.Forms.PictureBox imgQR;
+    }
+}
+
---
+++ ExerciciQRSysTray/fmMainQRSys.cs
@@ -0,0 +1,194 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using AForge.Video;                 // hem instal·lat els paquets (Nuget) Aforge, Aforge.Video i Aforge.Video.DirectShow
+using AForge.Video.DirectShow;
+
+using QRCoder;          // hem instal·lat el paquet (NuGet) QR Coder de Raffael Herrmann
+using ZXing;
+
+namespace ExerciciQRSysTray
+{
+    public partial class fmMainQRSys : Form
+    {
+        public fmMainQRSys()
+        {
+            InitializeComponent();
+        }
+
+        //Declareacio de les variables globals
+        FilterInfoCollection filtreCameres;
+        VideoCaptureDevice kam = null; //Aqui declarem la camara i la posem en null per no ocupar res
+        Bitmap bmp = null; //Aqui declarem el bitmap de la imatge que farem serbir en null per no ocupar res
+
+
+        //Declarem un lector de codiqr i posem la autorotacio del codi qr a true
+        BarcodeReader lector = new BarcodeReader { AutoRotate = true };
+
+        //Declarem la string de textcodiqr
+        string textcodiqr = null;
+        string textcodiqranterior = null; //Declarem aquesta variable per que aixi farem un control per evitar que s'obrin 30 mil pestanyes amb la mateixa informacio
+
+        
+        //Iniciem el nom de la camara a null
+        string nomcamara = null;
+
+        //Cada cop que apretem al boto de tancar el systray es tancara el programa
+        private void sortirToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            //Com que no volem que hi hagi cap error, tancarem la camara abans de tancar el programa
+            aturarcam(); 
+
+            //Aqui tanquem el programa amb normalitat
+            this.Close();
+        }
+
+        private void activarCamaraToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            
+            //Aquesta sera la funcio de obrir la camara
+            obrirCamara();
+        }
+
+        private void nouFotograma(object sender, NewFrameEventArgs eventArgs)
+        {
+            //Per cada fotograma que capti la camara cridara aquesta funcio
+
+            //Aqui guardem la imatge que ens arriba de la camara en el bitmap
+            bmp = (Bitmap)(Image)eventArgs.Frame.Clone();
+            //Declarem el texte del textbox a null
+            //Aquesta funcio no fa falta ja que no farem res amb el text que estigui adalt, ni el llegim ni tenim temps a mostrarlo per la velocitat dels frames
+            tbQR.Text = "";
+
+            var l = lector.Decode(bmp); //Aqui fa el decode de la imatge de la camara i busca un codi qr a la imatge
+            //IMPORTANT!: Si no hem detectat cap codi qr, el valor de "l" sera null, en cas contrari, si ha pogut llegir un qr retorna el seu valor corresponent i no null
+            if (l != null) 
+            {
+                //Aqui asignem el text del codiqr a la variable textcodiqr
+                textcodiqr = l.Text.ToString();
+
+                //Si la variable textcodiqr es diferent de la variable textcodiqranterior, vol dir que hem llegit un codiqr diferent, entrara a dins del if
+                if (textcodiqr != textcodiqranterior)
+                {
+                    //Arranquem un proces amb el text codi qr que ha trobat, com que sera sempre un link, al arrancar un proces i passarli un link automaticament t'obre el navegador web
+                    Process.Start(textcodiqr);
+
+                    //I assignem el valor de textcodi qr ACTUAL a text codi qr anterior perque no torni a entrar a dins del bucle
+                    textcodiqranterior = textcodiqr;
+                }
+                
+
+                
+            }
+            
+        }
+
+        private void obrirCamara()
+        {
+            //Aquesta es la funcio que farem servir per obrir les camares
+            kam = new VideoCaptureDevice(filtreCameres[0].MonikerString);
+            kam.NewFrame += nouFotograma;
+            kam.Start(); //Com que encara no tenim el programa acabat no executarem la camara fins que no vagi tot be
+        }
+
+        
+
+        private void fmMainQRSys_Load(object sender, EventArgs e)
+        {
+            //Cada cop que executem el programa, executarem la funcio de veure quines camares tenim disponibles
+            getCameres();
+        }
+
+        private void getCameres()
+        {
+            //Fem una array que ens mostrara totes les camares del ordinador
+            filtreCameres = new FilterInfoCollection(FilterCategory.VideoInputDevice);
+
+            //Per cada camara que trobi, farem una volta al bucle
+            foreach (FilterInfo p in filtreCameres)
+            {
+                //Si el nom de la camara ES DIFERENT a OBS Virtual Cam, entrara a dins del if
+                if (p.Name != "OBS Virtual Camera")
+                {
+                    //Aqui el valor de nomcamara ha de ser una merda generica com "HD Webcam" o alguna merda aixi
+                    nomcamara = (p.Name);
+                }
+                
+            }
+        }
+
+        private void desactivarCamaraToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            //Aquesta es la funcio que tancarem la camara
+            aturarcam();
+        }
+
+        private void aturarcam()
+        {
+            //Com que no sabem si la camara esta oberta o no, farem un if per comprovar-ho
+            if ((kam != null) && (kam.IsRunning))
+            {
+                //Si la camara esta encesa, la tancarem
+                kam.Stop();
+                //I declararem la KAM a null per no ocupar espai en ram
+                kam = null;
+            }
+        }
+
+        private void fmMainQRSys_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            aturarcam();
+        }
+
+        private void niIcona_MouseClick(object sender, MouseEventArgs e)
+        {
+            //Si la icona de angrybirds rep un click, entrara a dins d'aquesta funcio
+
+            //Si el boto que s'ha apretat és el boto esquerra entrara a dins del if
+            if (e.Button == MouseButtons.Left)
+            {
+                //Provant i fent varies coses he conseguit mostrar el menu de la app amb el boto esquerra
+                cmMenuStripIcona.Show(Cursor.Position);
+                //niIcona.ShowBalloonTip(3000);
+            }
+        }
+
+        private void generarQRToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            //Cada cop que apretem al boto de generar qr, obrira una nova finestra
+            fmMainQRSys fmMain = new fmMainQRSys();
+            fmMain.WindowState = FormWindowState.Normal;
+            fmMain.Visible = true;
+           
+        }
+
+        private void btCrearQR_Click(object sender, EventArgs e)
+        {
+            if (tbQR.Text.Trim() == "")
+            {
+                //Aqui entrara si no has posat cap text per convertir a qr
+                MessageBox.Show("Has d'introduir un text per a generar el QR", "ERROR", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else
+            {
+                //Aqui fem la generacio del codi qr
+                QRCodeGenerator qr = new QRCodeGenerator();
+                QRCodeData qrData = qr.CreateQrCode(tbQR.Text.Trim(), QRCodeGenerator.ECCLevel.Q);
+                QRCode qrCode = new QRCode(qrData);
+                Bitmap qrCodeImage = qrCode.GetGraphic(10);
+
+                imgQR.Image = qrCodeImage;
+                imgQR.SizeMode = PictureBoxSizeMode.StretchImage;
+            }
+        }
+    }
+}
